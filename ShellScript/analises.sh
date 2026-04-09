4c4c56d7cd9fdcce0fdc4aabfd7a756271d9fc5f
EnzoFerreira28 <enzosfcontato@gmail.com>
2026-04-09 14:06:35 -0300

    Script para analise de logs

---
+++ ShellScript/analises.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LOG="logs/nginx-access.log"
+
+echo "Top 5 IPs com mais requisições:"
+awk '{print $1}' $LOG | sort | uniq -c | sort -rn | head -5
+
+echo ""
+echo "Top 5 caminhos mais acessados:"
+awk '{print $7}' $LOG | sort | uniq -c | sort -rn | head -5
+
+echo ""
+echo "Top 5 códigos de status HTTP:"
+awk '{print $9}' $LOG | grep -E '^[0-9]+$'  | sort | uniq -c | sort -rn | head -5
+
+echo ""
+echo "Top 5 user agents:"
+awk -F'"' '{print $6}' $LOG | sort | uniq -c | sort -rn | head -5
