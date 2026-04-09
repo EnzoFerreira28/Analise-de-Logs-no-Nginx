7649794f8d53f96b1950137b373d5915b128a618
EnzoFerreira28 <enzosfcontato@gmail.com>
2026-04-09 14:20:08 -0300

    Deletando Script feito somente para testes

--- ShellScript/scriptTeste.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "Estou testando se consegui acessar corretamente uma subpasta do meu repositório no github
-e então utilizando o Neovim para criar um arquvio shell para commitar ele."
