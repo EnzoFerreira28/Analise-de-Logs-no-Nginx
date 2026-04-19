d9423eb4811f1d5ed39964378500a3afbcba943c
EnzoFerreira28 <enzosfcontato@gmail.com>
2026-04-19 18:54:43 -0300

    Melhoria no acesso aos arquivos .log

--- ShellScript/analises.sh
+++ ShellScript/analises.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-#Caso queira executar seus logs insira o caminho aqui
-#exemplo: /var/log/nginx/seuarquivo.log
-LOG=${1:-"logs/nginx-access.log"}
+# Pega o diretório do script atual
+#
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Define a pasta de logs como sendo "logs" no nível superior ao script
+#
+LOG="${1:-"$SCRIPT_DIR/../logs/nginx-access.log"}"
 
 echo "Top 5 IPs com mais requisições:"
 awk '{print $1}' $LOG | sort | uniq -c | sort -rn | head -5
